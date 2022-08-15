ff898c3d47c1e8671a6016c62e99c25aa08f4b0c
IFEDI MARCELINUS ONYECHE <ifedimarco85@gmail.com>
2022-08-15 08:03:13 +0100

    Add project files.

---
+++ Payment.Domain/Customer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Payment.Domain
+{
+    public class Customer
+    {
+        public int CustomerId { get; set; }
+        public string CustomerName { get; set; }
+        public string CustomerAccountNumber { get; set; }
+    }
+}
+
+
+/*
+ equest Parameter (json string)  {  "creditAccount":"5900174721",  "debitAccount":"1700313889",  
+"transactionAmount": "2000.45",  "currencyCode":"NGN",  "narration":"Testing",  "transactionReference":"20191119143501",  "userName":"test",  "password":"test"  }  
+ */
